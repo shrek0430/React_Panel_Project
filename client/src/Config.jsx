95a8991d81cbe0113de0f07fbfb19b58bb24e1d2
shrek0430 <206593158+shrek0430@users.noreply.github.com>
2025-02-10 10:25:17 +0530

    error

--- client/src/Config.jsx
+++ client/src/Config.jsx
@@ -2,25 +2,38 @@ import React from 'react';
 import axios from 'axios'; 
 
 export const BASE_URL = "http://localhost:8000/user";
-const token = localStorage.getItem('token');
+
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 1000,
-  headers: {
-    'Authorization': token ? `Bearer ${token}` : '',
-  },
 });
 
-axiosInstance.interceptors.response.use(
-  (response) => response, 
-  (error) => {
-    if (error.response && error.response.status === 403 && error.response.data.message === 'Token expired') {
-      localStorage.removeItem('token'); 
-      window.location.href = '/';
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
     }
-    return Promise.reject(error); 
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      const { status } = error.response;
+
+      if (status === 403) {
+        localStorage.removeItem('token');
+        window.location.replace('/');
+      }
+    }
+    return Promise.reject(error);
   }
 );
+
 
 const Config = () => {
   return (
